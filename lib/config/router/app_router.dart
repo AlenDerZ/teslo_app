f36b414628ba13e236e107d23c3f5c55ff7ca965
Alejandra Buentello <alenlbuentello7618@gmail.com>
2024-08-05 13:23:12 -0600

    GoRouterNotifier

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,31 +1,44 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_app/features/auth/auth.dart';
 import 'package:teslo_app/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
+import 'app_router_notifier.dart';
 
-    //* Primera Pantalla
-    GoRoute(  
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
-    //* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    //*Register Routes
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
-    //*Product Routes
-    GoRoute(
-      path: '/products',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ]
-  //! TODO: Bloquear si no se está autenticado de alguna manera
-);
+final goRouterProvider = Provider((ref){
+
+  final goRouterNotifier = ref.read(GoRouterNotifierProvider);
+
+  return GoRouter(
+    initialLocation: '/login',
+    refreshListenable: goRouterNotifier,
+    routes: [
+
+      //* Primera Pantalla
+      GoRoute(  
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
+      //* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      //*Register Routes
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
+      //*Product Routes
+      GoRoute(
+        path: '/products',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
+    
+    redirect: (context, state) {
+      print(state.matchedLocation);
+      return null;
+    },
+  );
+});
