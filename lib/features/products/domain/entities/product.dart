005e913ababe00bedcd29826249bfe39ad2cb9ab
Alejandra Buentello <alenlbuentello7618@gmail.com>
2024-08-16 16:08:48 -0600

    Product Mapper

--- lib/features/products/domain/entities/product.dart
+++ lib/features/products/domain/entities/product.dart
@@ -3,13 +3,13 @@ import 'package:teslo_app/features/auth/domain/domain.dart';
 class Products {
   String id;
   String title;
-  int price;
+  double price;
   String description;
   String slug;
   int stock;
   List<String> sizes;
-  Gender gender;
-  List<Tag> tags;
+  String gender;
+  List<String> tags;
   List<String> images;
   User? user;
 
@@ -26,28 +26,4 @@ class Products {
     required this.images,
     this.user,
   });
-
 }
-
-enum Gender {
-  KID,
-  MEN,
-  WOMEN
-}
-
-enum Tag {
-  SHIRT
-}
-
-enum Email {
-  TEST1_GOOGLE_COM
-}
-
-enum FullName {
-  JUAN_CARLOS
-}
-
-enum Role {
-  ADMIN
-}
-
--- lib/features/products/infrastructure/datasources/products_datasources_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasources_impl.dart
@@ -1,6 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_app/config/config.dart';
 import 'package:teslo_app/features/products/domain/domain.dart';
+import 'package:teslo_app/features/products/infrastructure/mappers/product_mapper.dart';
 
 class ProductsDatasourcesImpl extends ProductsDatasource {
+
+  late final Dio dio;
+  final String accesTocken;
+
+  ProductsDatasourcesImpl({
+    required this.accesTocken
+  }) : dio = Dio(BaseOptions(
+    baseUrl: Environment.apiUrl,
+    headers: {
+      'Authorization': 'Bearer $accesTocken'
+    }
+  ));
+
   @override
   Future<Products> createUpdateProduct(Map<String, dynamic> productLike) {
     // TODO: implement createUpdateProduct
@@ -14,9 +30,15 @@ class ProductsDatasourcesImpl extends ProductsDatasource {
   }
 
   @override
-  Future<List<Products>> getProductsByPage({int limit = 10, int offset = 0}) {
-    // TODO: implement getProductsByPage
-    throw UnimplementedError();
+  Future<List<Products>> getProductsByPage({int limit = 10, int offset = 0}) async {
+    final response = await dio.get<List>('/api/products?limit=$limit&offset=$offset');
+    final List<Products> products = [];
+
+    for (var product in response.data ?? []) {
+      products.add(ProductMapper.jsonToEntity(product));
+    }
+
+    return products;
   }
 
   @override
