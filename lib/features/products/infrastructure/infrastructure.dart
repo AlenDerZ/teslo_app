6479bfe08be19c9da6034d1f382a7381126bb4ab
Alejandra Buentello <alenlbuentello7618@gmail.com>
2024-08-16 17:11:47 -0600

    StateNotifierProvider Products

---
+++ lib/features/products/infrastructure/infrastructure.dart
@@ -0,0 +1,3 @@
+export 'package:teslo_app/features/products/infrastructure/datasources/products_datasources_impl.dart';
+export 'package:teslo_app/features/products/infrastructure/repositories/products_repository_impl.dart';
+export 'package:teslo_app/features/products/infrastructure/mappers/product_mapper.dart';
---
+++ lib/features/products/presentation/providers/providers.dart
@@ -0,0 +1,2 @@
+export 'package:teslo_app/features/products/presentation/providers/product_repository_provider.dart';
+export 'package:teslo_app/features/products/presentation/providers/products_provider.dart';
